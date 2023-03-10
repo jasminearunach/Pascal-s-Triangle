bf453135087533775b0b7d68e359ecfdcf7a52cb
jasminearunach <127538394+jasminearunach@users.noreply.github.com>
2023-03-10 09:13:23 -0800

    Add files via upload

---
+++ pascal.py
@@ -0,0 +1,18 @@
+from math import factorial
+def pascal_tri(numRows):
+  '''Print Pascal's triangle with numRows.'''
+  for i in range(numRows):
+    # loop to get leading spaces
+	  for j in range(numRows-i+1):
+		  print(end=" ")
+    
+    # loop to get elements of row i
+	  for j in range(i+1):
+		  # nCr = n!/((n-r)!*r!)
+		  print(factorial(i)//(factorial(j)*factorial(i-j)), end=" ")
+
+	 # print each row in a new line
+	  print("\n")
+
+if __name__ == '__main__':
+    pascal_tri(10) 
